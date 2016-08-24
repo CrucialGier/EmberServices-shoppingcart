36d1b145944f2f205ea2d1e8931229672f3b62dc
Stewart Cole and Terri Bradley <stewart+terri@>
2016-08-24 13:59:05 -0700

    autoatically update price when new items are added to cart

---
+++ app/services/shopping-cart.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  items: [],
+
+  add(item) {
+    this.get('items').pushObject(item);
+  },
+  totalPrice: Ember.computed('items.[]', function(){
+    var price = 0;
+    var items = this.get('items');
+    for (var i = 0; i < items.length; i++) {
+      price += items[i].get('cost');
+    }
+    return price.toFixed(2);
+  })
+});
