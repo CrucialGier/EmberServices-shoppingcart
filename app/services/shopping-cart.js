98232fce4dae3432fa02a7babeb0d0e524c76b38
Stewart Cole and Terri Bradley <stewart+terri@>
2016-08-25 10:43:49 -0700

    add cost in shopping cart/add new products component

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -6,6 +6,12 @@ export default Ember.Service.extend({
   add(item) {
     this.get('items').pushObject(item);
   },
+  remove(item) {
+    this.get('items').removeObject(item);
+  },
+  empty() {
+    this.get('items').clear();
+  },
   totalPrice: Ember.computed('items.[]', function(){
     var price = 0;
     var items = this.get('items');
